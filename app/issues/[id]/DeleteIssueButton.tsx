e7080d1ce5ff62de3e270207a730cc68a5727270
Anselm Jade Jamig <jadejamigdev@gmail.com>
2024-03-30 11:27:52 +0800

    show a confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,30 @@
+'use client';
+
 import { TrashIcon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 
 const EditIssueButton = () => {
   return (
-    <Button color='red'>
-        {/* <TrashIcon/> */}
-            Delete Issue
-    </Button>
+    <AlertDialog.Root>
+        <AlertDialog.Trigger>
+            <Button color='red'>Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+            <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+            <AlertDialog.Description>
+                Are you sure you want to delete this issue? This Action cannot be undone.
+            </AlertDialog.Description>
+            <Flex mt='4' gap='4'>
+                <AlertDialog.Cancel>
+                    <Button variant='soft' color='gray'>Cancel</Button>
+                </AlertDialog.Cancel>
+                <AlertDialog.Action>
+                    <Button color='red'>Delete Issue</Button>
+                </AlertDialog.Action>
+            </Flex>
+        </AlertDialog.Content>
+    </AlertDialog.Root>
+
   )
 }
 
